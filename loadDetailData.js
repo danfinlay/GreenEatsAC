d68993dbfc80212ad0ac95f70c4e3098b97eeeae
flyswatter <somniac@me.com>
2012-12-08 15:52:31 -0800

    Detail view working.

--- loadDetailData.js
+++ loadDetailData.js
@@ -1,26 +1,41 @@
 
-
-$.on("load", function(event, ui){
-	
-		var urlData = getUrlVars();
-		var locationId = unescape(urlData["id"]);
-		var items = [];
-		var ask = new String();
-		
-		if(violation.violations){
-					ask = '<img src="skeptical.png">';
-					} else {
-					ask = '<img src="good.png">';
-					};
-    	//$.each(data, function(key, val) {
-    	//items.push('<li id="' + key + '">' + val + '</li>');
-		$.each(json, function(){
-
-
-		
-		items.push('<li><h3>' + violation.name +'</h3><p>' + violation.adress + '</p><p>' + violation.city + '</p><p class="ui-li-aside">' + ask + '</p></a></li>');
-	
+var urlData = getUrlVars();
+var locationId = unescape(urlData["id"]);
+var items = [];
+var ask = new String();
+console.log("Starting..");
+var violation;
+violations.forEach(function(checker){
+	if(checker.name===locationId){
+		console.log("Found id:");
+		violation = checker;
+	}
 });
+
+if(violation!==undefined){
+	if(violation.violations){
+		ask = '<img src="skeptical.png">';
+	} else {
+		ask = '<img src="good.png">';
+	};
+	//$.each(data, function(key, val) {
+	//items.push('<li id="' + key + '">' + val + '</li>');
+	//for(thing in violation){
+
+		//console.log("Loading "+thing+" which is "+violation[thing]);
+	
+		//$('ul').append('<li><a>'+thing+': '+violation[thing]+'</a></li>');
+			
+		$('body').append('<h3>' + violation.name +'</h3><p>' + violation.adress + '</p><p>' + violation.city + '</p><p class="ui-li-aside">' + ask + '</p></a>');
+
+
+		//$('ul').append('<li><h3>' + violation.name +'</h3><p>' + violation.adress + '</p><p>' + violation.city + '</p><p class="ui-li-aside">' + ask + '</p></a></li>');
+	//};
+}else{
+	console.log("Violation not found.");
+}
+
+		
 
 
 function getUrlVars()
